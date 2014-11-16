7fcf44b4b3a035aa6d4665b306b1d661b040fedb
Jiangge Zhang <tonyseek@gmail.com>
2014-11-16 17:09:04 +0800

    Bump version: 0.4 → 0.5

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('requirements.txt') as requirements:
 
 setup(
     name='pastry',
-    version='0.4',
+    version='0.5',
     author='Jiangge Zhang',
     author_email='tonyseek@gmail.com',
     url='https://github.com/szulabs/pastry',
