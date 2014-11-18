74903ac4a729a37f9fb3de6518ace0b8dd09e573
shonenada <shonenada@gmail.com>
2014-11-18 11:19:01 +0800

    Bump version: 0.7 → 0.8

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('requirements.txt') as requirements:
 
 setup(
     name='pastry',
-    version='0.7',
+    version='0.8',
     author='Jiangge Zhang',
     author_email='tonyseek@gmail.com',
     url='https://github.com/szulabs/pastry',
